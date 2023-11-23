b434a65bce19474ec53aca637f4806ff64403642
Kacper Pilarski <xxxxbaobabxxxx@protonmail.com>
2023-11-23 14:03:32 +0100

    fix js

--- engine.js
+++ engine.js
@@ -1,32 +1,44 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let buttonElement = document.querySelector(".js-button");
-let resultElement = document.querySelector(".js-result");
 
-let rateGBP = 4.98;
-let rateEUR = 4.35;
-let rateDolar = 3.98;
+  let formElement = document.querySelector(".js-form");
+  let amountElement = document.querySelector(".js-amount");
+  let currencyElement = document.querySelector(".js-currency");
+  let resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
+  let rateGBP = 4.98;
+  let rateEUR = 4.35;
+  let rateDolar = 3.98;
 
-  let result;
-  switch (currency) {
-    case "GBP":
-      result = amount / rateGBP;
-      break;
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
 
-    case "EUR":
-      result = amount / rateEUR;
-      break;
+    let result;
+    switch (currency) {
+      case "GBP":
+        result = amount / rateGBP;
+        break;
 
-    case "Dolar":
-      result = amount / rateDolar;
-      break;
-  }
+      case "EUR":
+        result = amount / rateEUR;
+        break;
 
-  resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
+      case "Dolar":
+        result = amount / rateDolar;
+        break;
+    }
+
+    resultElement.innerHTML = `${amount.toFixed(
+      2
+    )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+  });
+
+  let elementButton = document.querySelector(".js-buttonDayNight");
+  let elementBody = document.querySelector(".js-body");
+  let elementDayNightVariable = document.querySelector(".js-dayNightVariable");
+
+  elementButton.addEventListener("click", () => {
+    elementBody.classList.toggle("body--dark");
+    elementDayNightVariable.innerText = elementBody.classList.contains("body--dark") ? "jasny" : "ciemny";
+  });
+
