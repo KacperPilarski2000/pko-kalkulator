c49897064d45b843a72665a97269f2b3ac53e199
Kacper Pilarski <xxxxbaobabxxxx@protonmail.com>
2023-11-24 21:49:41 +0100

    add prettier formatter and litte chang in js

--- engine.js
+++ engine.js
@@ -1,48 +1,50 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
 
-  let formElement = document.querySelector(".js-form");
-  let amountElement = document.querySelector(".js-amount");
-  let currencyElement = document.querySelector(".js-currency");
-  let resultElement = document.querySelector(".js-result");
+let rateGBP = 4.98;
+let rateEUR = 4.35;
+let rateDolar = 3.98;
 
-  let rateGBP = 4.98;
-  let rateEUR = 4.35;
-  let rateDolar = 3.98;
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
 
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
+  let result;
+  switch (currency) {
+    case "GBP":
+      result = amount / rateGBP;
+      break;
 
-    let result;
-    switch (currency) {
-      case "GBP":
-        result = amount / rateGBP;
-        break;
+    case "EUR":
+      result = amount / rateEUR;
+      break;
 
-      case "EUR":
-        result = amount / rateEUR;
-        break;
+    case "Dolar":
+      result = amount / rateDolar;
+      break;
+  }
 
-      case "Dolar":
-        result = amount / rateDolar;
-        break;
-    }
+  resultElement.innerHTML = `${amount.toFixed(
+    2
+  )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+});
+let calculatButton = document.querySelector(".js-button");
+let elementButton = document.querySelector(".js-buttonDayNight");
+let elementBody = document.querySelector(".js-body");
+let elementDayNightVariable = document.querySelector(".js-dayNightVariable");
 
-    resultElement.innerHTML = `${amount.toFixed(
-      2
-    )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-  });
+elementButton.addEventListener("click", () => {
+  elementBody.classList.toggle("body--dark");
+  elementDayNightVariable.innerText = elementBody.classList.contains(
+    "body--dark"
+  )
+    ? "jasny"
+    : "ciemny";
+});
 
-  let elementButton = document.querySelector(".js-buttonDayNight");
-  let elementBody = document.querySelector(".js-body");
-  let elementDayNightVariable = document.querySelector(".js-dayNightVariable");
-
-  elementButton.addEventListener("click", () => {
-    elementBody.classList.toggle("body--dark");
-    elementDayNightVariable.innerText = elementBody.classList.contains("body--dark") ? "jasny" : "ciemny";
-  });
-
-
-formElement.addEventListener("click", () => {
-resultElement.classList.toggle("result");
+calculatButton.addEventListener("click", () => {
+  resultElement.classList.toggle("result");
 });
