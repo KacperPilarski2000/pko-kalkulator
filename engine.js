e4fc018815621e619800667d6fa7dadf584b2101
Kacper Pilarski <xxxxbaobabxxxx@protonmail.com>
2023-12-01 23:23:55 +0100

    add function to js

--- engine.js
+++ engine.js
@@ -1,53 +1,64 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
-let currency = currencyElement.value;
+const calculation = (currency, amount) => {
+  const GBPValue = 5.04;
+  const EuroValue = 4.35;
+  const DolarValue = 3.98;
 
-
-let rateGBP = 4.98;
-let rateEUR = 4.35;
-let rateDolar = 3.98;
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
-
-  let result;
   switch (currency) {
     case "GBP":
-      result = amount / rateGBP;
-      break;
-
+      return amount / GBPValue;
     case "EUR":
-      result = amount / rateEUR;
-      break;
+      return amount / EuroValue;
 
     case "Dolar":
-      result = amount / rateDolar;
-      break;
+      return amount / DolarValue;
   }
-  
-  resultElement.innerHTML = resultElement.classList.contains("result")?`${amount.toFixed(
-    2
-  )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`
-});
-let calculatButton = document.querySelector(".js-button");
-let elementButton = document.querySelector(".js-buttonDayNight");
-let elementBody = document.querySelector(".js-body");
-let elementDayNightVariable = document.querySelector(".js-dayNightVariable");
+};
 
-elementButton.addEventListener("click", () => {
+const dayNightEffect = (event) => {
+  event.preventDefault();
+  const elementBody = document.querySelector(".js-body");
+  const elementDayNightVariable = document.querySelector(
+    ".js-dayNightVariable"
+  );
+
   elementBody.classList.toggle("body--dark");
   elementDayNightVariable.innerText = elementBody.classList.contains(
     "body--dark"
   )
     ? "jasny"
     : "ciemny";
-});
+};
 
-calculatButton.addEventListener("click", () => {
-  resultElement.classList.toggle("result");
-});
-.addEventListener
+const resultUpdateText = (amount, result, currency) => {
+  const resultElement = document.querySelector(".js-result");
+
+  resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+  resultElement.classList.add("result");
+}
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+
+  const currency = currencyElement.value;
+  const amount = +amountElement.value;
+
+  const result = calculation(currency, amount);
+
+  resultUpdateText(amount, result, currency);
+};
+
+const init2 = () => {
+  const formElement = document.querySelector(".js-form");
+
+  formElement.addEventListener("submit", onFormSubmit);
+};
+const init = () => {
+  const elementButton = document.querySelector(".js-buttonDayNight");
+
+  elementButton.addEventListener("click", dayNightEffect);
+};
+
+init();
+init2();
